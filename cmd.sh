72027dbded196f862a18e60be17b65d79d08c8c1
seohasong <tisutoo@gmail.com>
2020-02-05 02:53:35 +0900

    continue previous working

--- cmd.sh
+++ cmd.sh
@@ -1,13 +1,16 @@
 main() {
-    export CMD=shs
     local args=( $@ )
     local nxt_args=${args[@]:1}
     local dir_path=$( cd $( dirname "$BASH_SOURCE" ) && pwd )
     local file=$dir_path/cmd/$args/main.sh
+
     if [ "${dir_path##*/}" == "CORE" ]
     then . "$dir_path/cmd/__reload__/main.sh" ""
-    else eval "$CMD() { . $dir_path/cmd.sh \$@; } && export -f $CMD"
+    else
+        if [ -z $CMD ]; then CMD=cmd; fi
+        eval "$CMD() { . $dir_path/cmd.sh \$@; } && export -f $CMD"
     fi
+
     if [ -f "$file" ]
     then
         if [[ "$args" =~ ^__ ]]

3bb6f7b19a4d5db0745a339192b3744aab0eb6f0
seohasong <tisutoo@gmail.com>
2020-02-24 04:55:17 +0900

    continue previous working

--- cmd.sh
+++ cmd.sh
@@ -6,8 +6,9 @@ main() {
     if [ -d "$adpath/core" ]; then core=core; fi
     local nxt_args="$core ${args[@]}"
     . "$adpath/$core/tools/cmd/init.sh" "$nxt_args"
+    nxt_args="${args[@]:1}"
     if [ -f "$APATH" ]
-    then $CMD "$APATH"
+    then $CMD "$APATH" "$nxt_args"
     elif [ -f "$adpath/core/cmd/$arg/main.sh" ]
     then . "$adpath/core/cmd.sh" ${args[@]}
     else . "$adpath/$core/tools/cmd/help.sh" "$adpath"

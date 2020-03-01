df78f7430335645a90a328a0dda0fc7998d47bb2
seohasong <tisutoo@gmail.com>
2020-03-02 01:27:39 +0900

    continue previous working

--- cmd.sh
+++ cmd.sh
@@ -2,13 +2,13 @@ main() {
     local args=( $@ )
     local arg=${args[0]}
     local adpath=$( dirname "${BASH_SOURCE:-${(%):-%x}}" )
-    local core=. cmd=$SHELL
+    local core=. cmd='bash -c'
     if [ -d "$adpath/core" ]; then core=core; fi
-    if [[ "$arg" =~ ^__.*__$ ]]; then cmd=.; fi
+    if [[ "$arg" =~ ^__.*__$ ]]; then cmd=eval; fi
     local nxt_args="$core ${args[@]}"
     . $adpath/$core/tools/cmd/init.sh "$nxt_args"
     if [ -f "$__APATH__" ]
-    then $cmd $adpath/$core/tools/cmd/exec.sh 
+    then $cmd 'main() { . $__APATH__; }; main'
     elif [ -f $adpath/core/cmd/$arg/main.sh ]
     then . $adpath/core/cmd.sh ${args[@]}
     else . $adpath/$core/tools/cmd/help.sh $adpath

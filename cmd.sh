1a521556a4adcd8cc2b6c9d20d4b84bf724a0633
seohasong <tisutoo@gmail.com>
2020-03-03 01:47:16 +0900

    continue previous working

--- cmd.sh
+++ cmd.sh
@@ -7,8 +7,8 @@ main() {
     if [[ "$arg" =~ ^__.*__$ ]]; then cmd=eval; fi
     local nxt_args="$adpath ${args[@]}"
     . $adpath/$core/tools/cmd/init.sh "$nxt_args"
-    if [ -f "$__APATH__" ]
-    then $cmd 'main() { . $__APATH__; }; main'
+    if [ -f $adpath/cmd/$arg/main.sh ]
+    then $cmd "main() { . $adpath/cmd/$arg/main.sh; }; main"
     elif [ -f $adpath/core/cmd/$arg/main.sh ]
     then . $adpath/core/cmd.sh ${args[@]}
     else . $adpath/$core/tools/cmd/help.sh

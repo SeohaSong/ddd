b4dee8f7110711fe456124c1e047f0a2d937924d
seohasong <tisutoo@gmail.com>
2020-03-03 01:40:41 +0900

    continue previous working

--- tools/cmd/close.sh
+++ tools/cmd/close.sh
@@ -1,8 +1,11 @@
 main() {
+    unset __args__
+    unset __opts__
+
     unset __CMD_PATH__
     unset __CMD_NAME__
     unset __ADPATH__
     unset __APATH__
-    unset __args__
+    unset __KEY__
 }
 main $1
--- tools/cmd/init.sh
+++ tools/cmd/init.sh
@@ -5,18 +5,19 @@ main() {
 
     local adpath=$( cd $dpath && pwd )
     export __args__=${args[@]:2}
+    export __opts__
 
     export __APATH__=$adpath/cmd/$arg/main.sh
     export __ADPATH__=$( dirname "$__APATH__" )
     export __CMD_PATH__=$adpath
     if [ ! -d $adpath/core ]; then export __CMD_PATH__=$( cd $adpath/.. && pwd );fi
-
     if [ ! -f $__CMD_PATH__/env/CMD ]; then echo cmd > $__CMD_PATH__/env/CMD; fi
     local cmd_name=$( cat $__CMD_PATH__/env/CMD 2> /dev/null || : )
     export __CMD_NAME__=$cmd_name
     if [ ! -z "$cmd_name" ]
     then eval "$cmd_name() { . $__CMD_PATH__/cmd.sh \$@; } && export -f $cmd_name"
     fi
+    export __KEY__=$( cat "$__CMD_PATH__/env/KEY" 2> /dev/null || : )
 
     local file dir
     for file in cmd.sh .gitignore

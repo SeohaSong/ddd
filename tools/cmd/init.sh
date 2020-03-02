9327dbb9c8ac2850cb4c681ea1ff7b4b7975177e
seohasong <tisutoo@gmail.com>
2020-03-02 20:49:43 +0900

    continue previous working

--- tools/cmd/init.sh
+++ tools/cmd/init.sh
@@ -1,25 +1,37 @@
 main() {
     local args=( $@ )
-    local adpath=${args[0]}
+    local dpath=${args[0]}
     local arg=${args[1]}
-    export __args__=${args[@]:2}
-    export __CMD_PATH__=$adpath
-    export __APATH__=$adpath/cmd/$arg/main.sh
-    export __ADPATH__=$( dirname "$__APATH__" )
 
-    if [ ${adpath##*/} == core ]; then adpath=$adpath/..; fi
-    local file
-    for file in cmd.sh .gitignore; do cp "$adpath/core/$file" "$adpath"; done
-    local dir 
+    export __args__=${args[@]:2}
+
+    export __CMD_PATH__=$( cd $dpath && pwd )
+    export __APATH__=$__CMD_PATH__/cmd/$arg/main.sh
+    export __ADPATH__=$( dirname "$__APATH__" )
+    export __MAIN_PATH__=$__CMD_PATH__
+    if [ -d $__CMD_PATH__/core ]
+    then export __MAIN_PATH__=$( cd $__CMD_PATH__/.. && pwd )
+    fi
+
+    local file dir
+    for file in cmd.sh .gitignore
+    do cp "$__MAIN_PATH__/core/$file" "$__MAIN_PATH__"
+    done
     for dir in data cmd env tools tools/src
     do
-        if [ ! -d $adpath/$dir ]; then mkdir $adpath/$dir; fi
-        if [ ! -f $adpath/$dir/.gitkeep ]; then touch $adpath/$dir/.gitkeep; fi
+        if [ ! -d $__MAIN_PATH__/$dir ]
+        then mkdir $__MAIN_PATH__/$dir
+        fi
+        if [ ! -f $__MAIN_PATH__/$dir/.gitkeep ]
+        then touch $__MAIN_PATH__/$dir/.gitkeep
+        fi
     done
-
-    if [ ! -f $adpath/env/cmd ]; then echo cmd > $adpath/env/cmd; fi
-    local name=$( cat $adpath/env/CMD 2> /dev/null || : )
-    local cmd="$name() { . $adpath/cmd.sh \$@; } && export -f $name"
+    
+    if [ ! -f $__MAIN_PATH__/env/CMD ]
+    then echo cmd > $__MAIN_PATH__/env/CMD
+    fi
+    local name=$( cat $__MAIN_PATH__/env/CMD 2> /dev/null || : )
+    local cmd="$name() { . $__MAIN_PATH__/cmd.sh \$@; } && export -f $name"
     if [ ! -z "$name" ]; then eval "$cmd"; fi
 }
 main $1

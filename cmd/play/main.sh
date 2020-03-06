6d6f443541253caeebbd2f44d620513a648c28c4
seohasong <tisutoo@gmail.com>
2020-03-06 22:36:15 +0900

    continue previous working

--- cmd/play/main.sh
+++ cmd/play/main.sh
@@ -1,17 +1,18 @@
 local CMD_PATH=$__CMD_PATH__
 local CMD=$__CMD__
 $CMD __trap__
-[[ "$PWD" =~ "$CMD_PATH" ]]
+
+local port=2222
 local net_opt='--network host'
 if $( $CMD __is-wsl__ )
-then net_opt='--publish 22:2222 --publish 4200:4200 --publish 8888:8888'
+then net_opt="--publish $port:$port --publish 4200:4200 --publish 8888:8888"
 fi
 $CMD docker run --detach --interactive --rm \
 --volume ${CMD_PATH#/mnt}:/home/ddd/${CMD_PATH##*/} \
 --name ddd \
 $net_opt \
 ddd
-echo Port 2222 | $CMD docker exec --interactive \
+echo Port $port | $CMD docker exec --interactive \
 --user root \
 ddd tee /etc/ssh/sshd_config
 $CMD docker exec --user root ddd service ssh start

0d670bd5be1b7f6e274f8b79ce5380ae36d1dd81
seohasong <tisutoo@gmail.com>
2020-03-11 01:17:41 +0900

    continue previous working

--- cmd/login/main.sh
+++ cmd/login/main.sh
@@ -1,5 +1,12 @@
-ssh ddd@localhost -t -p 2222 "
-    . .profile
-    cd ${DDD_PATH##*/}/${PWD#$DDD_PATH}
-    bash
-"
+[[ $PWD =~ $DDD_PATH ]]
+path=${PWD#$DDD_PATH}
+path=${DDD_PATH##*/}/${path#/}
+path=${path%/}
+ddd_file=${DDD_PATH##*/}/ddd/cmd.sh
+ssh ddd@localhost -t -p 2222 "\
+. .profile
+. $ddd_file __profile__
+cd $path
+bash"
+
+# key toss

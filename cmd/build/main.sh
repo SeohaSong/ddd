fb999261ddd66202efd1f4cd0916c4dd26fd3942
seohasong <tisutoo@gmail.com>
2020-03-12 23:41:45 +0900

    continue previous working

--- cmd/build/main.sh
+++ cmd/build/main.sh
@@ -4,7 +4,7 @@ name=${file%.*} ext=${file##*.}
 case $ext in
 py)
     mkdir __tmp__
-    cp $file __tmp__
+    for v in $( ls | grep -v __tmp__ ); do cp -r $v __tmp__; done
     cd __tmp__
     pkgs="pkg_resources.py2_warn"
     for pkg in $pkgs; do import_opt="$import_opt --hidden-import $pkg"; done

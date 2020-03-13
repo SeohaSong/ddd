6f2e02c3ff12b828674ce7f5363e55b65b7faf37
seohasong <tisutoo@gmail.com>
2020-03-14 01:00:26 +0900

    continue previous working

--- cmd/verify/main.sh
+++ cmd/verify/main.sh
@@ -4,5 +4,5 @@ for ver in 16.04 18.04; do
     chk=$( $DDD docker images | grep -E "ubuntu.+${ver%.04}" || : )
     if [ -z "$chk" ]; then $DDD docker pull ubuntu:$ver; fi
     $DDD echo $ver
-    $DDD play_ ubuntu:$ver $ARGS
+    $DDD _play ubuntu:$ver $ARGS
 done

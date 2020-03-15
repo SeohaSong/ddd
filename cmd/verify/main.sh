cb4e03c6dd5a0e0050b609257145c87cb5b1c270
seohasong <tisutoo@gmail.com>
2020-03-16 04:49:51 +0900

    continue previous working

--- cmd/verify/main.sh
+++ cmd/verify/main.sh
@@ -1,4 +1,4 @@
-[ ! -z "$@" ]
+[[ ! -z "$@" ]]
 for ver in 16.04 18.04; do
     chk=$( $DDD docker images | grep -E "ubuntu.+${ver%.04}" || : )
     if [ -z "$chk" ]; then $DDD docker pull ubuntu:$ver; fi

20f653a04b83bc4e8d637f10f6732e1c1ad88d27
seohasong <tisutoo@gmail.com>
2020-06-24 20:08:05 +0900

    continue previous working

--- cmd/run/main.sh
+++ cmd/run/main.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
 [[ "$PWD" =~ "$DDD_PATH" ]]
-if $DDD __is-ddd__; then $@; else
-    $DDD docker exec --tty --interactive \
-    --workdir /home/ddd/DDD/${PWD#$DDD_PATH} ddd $@; fi
+
+if $DDD __is-ddd__; then
+    $@
+else
+    local opt=
+    if [[ -t 1 ]]; then
+        opt='--tty --interactive'; fi
+    $DDD docker exec $opt --workdir /home/ddd/DDD/${PWD#$DDD_PATH} ddd $@
+fi

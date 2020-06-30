7a46e15e9bbc3863b955ff0ebbb51dac1a19ac67
seohasong <tisutoo@gmail.com>
2020-07-01 02:04:33 +0900

    continue previous working

--- cmd/_play/main.sh
+++ cmd/_play/main.sh
@@ -1,12 +1,17 @@
 name=${1%%:*}
-path=. gpu_opt= name_opt="--name $name" etc_opt="--tty"
-net_opt='--publish 2222:2222 --publish 8888:8888 --publish 4200:4200'
+path=.
 if [[ $PWD != $DDD_PATH && $PWD =~ $DDD_PATH ]]; then
     path=${PWD#$DDD_PATH/}; fi
-if ! $DDD __is-wsl__; then gpu_opt='--gpus all' net_opt='--network host'; fi
+# gpu_opt=
+# gpu_opt='--gpus all'
+net_opt='--publish 2222:2222 --publish 8888:8888 --publish 4200:4200'
+if ! $DDD __is-wsl__; then
+    net_opt='--network host'; fi
+etc_opt="--tty"
 if [[ $name == ddd ]]; then etc_opt="--detach"; fi
 $DDD docker run \
-    $gpu_opt $net_opt $name_opt $etc_opt \
+    $gpu_opt $net_opt $etc_opt \
     --interactive --rm \
+    --name $name \
     --workdir /home/ddd/DDD/$path \
     --volume ${DDD_PATH#/mnt}:/home/ddd/DDD $@
--- cmd/login/main.sh
+++ cmd/login/main.sh
@@ -1,4 +1,4 @@
-local opt=
-if [[ -t 1 ]]; then
-    opt='--tty --interactive'; fi
-$DDD docker exec $opt --workdir /home/ddd/DDD/${PWD#$DDD_PATH} ddd bash
+$DDD docker exec \
+    --tty --interactive \
+    --workdir /home/ddd/DDD/${PWD#$DDD_PATH} \
+    ddd bash

f60cf821c0d161773445d66954379d3c3f5a608a
seohasong <tisutoo@gmail.com>
2020-07-01 02:33:55 +0900

    continue previous working

--- cmd/login/main.sh
+++ cmd/login/main.sh
@@ -1,4 +1,3 @@
-$DDD docker exec \
-    --tty --interactive \
-    --workdir /home/ddd/DDD/${PWD#$DDD_PATH} \
-    ddd bash
+path=${PWD#$DDD_PATH/}
+if [[ $PWD == $DDD_PATH || ! $PWD =~ $DDD_PATH ]]; then path=.; fi
+ssh ddd@127.0.0.1 -t -p 2222 "cd DDD/$path && bash"

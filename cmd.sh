cfab3e8f61dcd930afda999d6f3d62ad103d0348
seohasong <tisutoo@gmail.com>
2020-07-17 11:38:01 +0900

    continue previous working

--- cmd.sh
+++ cmd.sh
@@ -2,22 +2,41 @@ main() {
     local args=( $@ )
     local arg=${args[0]}
     local nxt_args=${args[@]:1}
-    local INTRO='
-        file=${BASH_SOURCE:-${(%):-%x}};
-        echo $( cd $( dirname $file ) && pwd )/${file##*/}'
-    local DDD_PATH=$( dirname $( dirname $( eval $INTRO ) ) )
-    local DDD=$( cat $DDD_PATH/env/DDD 2> /dev/null || echo ddd )
+    local DDD_PATH=$( dirname $( cd $( dirname $BASH_SOURCE ) && pwd ) )
+    local DDD=$( cat $DDD_PATH/env/DDD 2> /dev/null )
+    DDD=${DDD:-"ddd"}
     eval "$DDD() { . $DDD_PATH/ddd/cmd.sh \$@; } && export -f $DDD"
-    if [[ -z $CHK ]]; then local CHK=$DDD && $DDD ____ && $DDD _____; fi
+
+    if [[ -z $CHK ]]
+    then
+        local CHK=$DDD
+        $DDD ____
+        $DDD _____
+    fi
+
     local file=$DDD_PATH/cmd/$arg/main.sh
-    if [ ! -f $file ]; then file=$DDD_PATH/ddd/cmd/$arg/main.sh; fi
-    if [ ! -f $file ]; then $DDD help; else
-        local cmd=". $file $nxt_args"
-        if [ -z "$nxt_args" ]; then cmd=". $file ''"; fi
-        if [[ ! "$arg" =~ ^__.*__$ ]]; then
-            cmd="$DDD __trap__; $cmd 2>&1"
-            cmd="( $DDD __init__ $nxt_args; $DDD __init___ $nxt_args; $cmd )"
-            cmd="$cmd 2> /dev/null"; fi
-        eval $cmd; fi
+    if [ ! -f $file ]
+    then
+        file=$DDD_PATH/ddd/cmd/$arg/main.sh
+    fi
+    if [ ! -f $file ]
+    then
+        $DDD help
+        return $?
+    fi
+
+    local cmd=". $file $nxt_args"
+    if [ -z "$nxt_args" ]
+    then
+        cmd=". $file ''"
+    fi
+    if [[ ! "$arg" =~ ^__.*__$ ]]
+    then
+        cmd="$DDD __trap__; $cmd 2>&1"
+        cmd="( $DDD __init__ $nxt_args; $DDD __init___ $nxt_args; $cmd )"
+        cmd="$cmd 2> /dev/null"
+    fi
+    eval $cmd
+    return $?
 }
 main $@

2d43434701a0bda2efba9746f36d4274f62bb358
seohasong <tisutoo@gmail.com>
2020-07-20 00:49:23 +0900

    continue previous working

--- cmd/setup/main.sh
+++ cmd/setup/main.sh
@@ -1,9 +1,13 @@
 [ ! -z "$KEY" ]
 [ ! -z "$PASSWORD" ]
-apath=$( dirname $( eval $INTRO ) )
-cd $apath
-if [ -d __tmp__ ]; then rm -rf __tmp__; fi
+
+cd $( dirname $BASH_SOURCE )
+if [ -d __tmp__ ]
+then
+    rm -rf __tmp__
+fi
 mkdir __tmp__
+
 file=__tmp__/dockerfile
 cp dockerfile $file
 txt=$( cat $file )
@@ -11,7 +15,13 @@ echo "$txt" | sed -e "s~<KEY\/>~$KEY~g" | tee $file
 txt=$( cat $file )
 echo "$txt" | sed -e "s/<PASSWORD\/>/$PASSWORD/g" | tee $file
 txt=$( cat $file )
-net_opt='--network host'
-if $DDD __is-wsl__; then net_opt=; fi
+
+net_opt=
+if ! $DDD .is-wsl
+then
+    net_opt='--network host'
+fi
+
 $DDD docker build --tag ddd $net_opt __tmp__
+
 rm -r __tmp__

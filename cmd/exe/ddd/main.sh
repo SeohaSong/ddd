f3d3b9528643fc722846c6b8049c16656e1dc586
seohasong <tisutoo@gmail.com>
2020-07-24 00:54:26 +0900

    continue previous working

--- cmd/exe/ddd/main.sh
+++ cmd/exe/ddd/main.sh
@@ -1,8 +1,8 @@
 name=ddd
 path=.
-net_opt='--publish 2222:2222 \
-         --publish 8000:8000 \
-         --publish 8100:8100 \
+net_opt='--publish 2222:2222
+         --publish 8000:8000
+         --publish 8100:8100
          --publish 8888:8888'
 etc_opt="--tty --detach"
 # gpu_opt='--gpus all'
@@ -17,12 +17,13 @@ then
     net_opt='--network host'
 fi
 
-docker run $gpu_opt $net_opt $etc_opt \
-                --interactive --rm \
-                --name $name \
-                --workdir /home/ddd/DDD/$path \
-                --volume $DDD_PATH:/home/ddd/DDD \
-                $name
+docker run \
+    $gpu_opt $net_opt $etc_opt \
+    --interactive --rm \
+    --name $name \
+    --workdir /home/ddd/DDD/$path \
+    --volume $DDD_PATH:/home/ddd/DDD \
+    $name
 
 echo Port 2222 | \
     docker exec --interactive --user root ddd tee /etc/ssh/sshd_config

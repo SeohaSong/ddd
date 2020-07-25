25d0be6079c0e5c1e32579f258d0ee5caa6cc30c
seohasong <tisutoo@gmail.com>
2020-07-25 08:20:08 +0000

    continue previous working

--- cmd/build/ddd/main.sh
+++ cmd/build/ddd/main.sh
@@ -2,19 +2,19 @@
 [ ! -z "$PASSWORD" ]
 
 cd $( dirname $BASH_SOURCE )
+
 if [ -d __tmp__ ]
 then
     rm -rf __tmp__
 fi
 mkdir __tmp__
 
+echo $KEY | tee __tmp__/KEY > /dev/null
+
 file=__tmp__/dockerfile
 cp dockerfile $file
 txt=$( cat $file )
-echo "$txt" | sed -e "s~<KEY\/>~$KEY~g" | tee $file
-txt=$( cat $file )
 echo "$txt" | sed -e "s/<PASSWORD\/>/$PASSWORD/g" | tee $file
-txt=$( cat $file )
 
 net_opt=
 if ! $DDD .is-wsl
